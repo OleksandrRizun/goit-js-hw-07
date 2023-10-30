34ef8c1929ba756eb5009fab67a6a337b928417a
OleksandrRizun <137624723+OleksandrRizun@users.noreply.github.com>
2023-10-30 19:16:00 +0200

    HW ver2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,21 +6,21 @@ let instance;
 
 // Gallery markup
 function createGalleryItem (preview, original, description) {
-    const galleryItem = document.createElement ("li");
-    galleryItem.classList.add("gallery__item");
-    galleryItem.innerHTML=
-    `<a class="gallery__link" href=${original}>
-        <img
-        class="gallery__image"
-        src=${preview}
-        alt=${description} />
-    </a>`
+    const galleryItem =
+    `<li class="gallery__item">
+        <a class="gallery__link" href=${original}>
+            <img
+            class="gallery__image"
+            src=${preview}
+            alt=${description} />
+        </a>
+    </li>`
     return galleryItem
 }
-const galleryContent = galleryItems.map(({preview, original, description}) => createGalleryItem(preview, original, description))
+const galleryContent = galleryItems.map(({preview, original, description}) => createGalleryItem(preview, original, description)).join("\n");
 
-const galleryElement = document.getElementsByClassName ("gallery")[0];
-galleryElement.append (...galleryContent);
+const galleryElement = document.querySelector (".gallery");
+galleryElement.insertAdjacentHTML ("beforeend", galleryContent);
 
 instance = new SimpleLightbox('.gallery a', {
     captionsData: "alt",
